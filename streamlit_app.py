984ae5b875abf53be76c9679d6d257b8be58a97c
yatishsingla <61521076+yatishsingla@users.noreply.github.com>
2023-02-20 10:18:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,9 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
+  df2[0],
+  width=400,
+  caption= product_caption
 )
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
